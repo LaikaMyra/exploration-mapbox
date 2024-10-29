ba4e9ba9ab13cdc0fd2c46891bf5c06d5d1c1e0c
LaikaMyra <156817447+LaikaMyra@users.noreply.github.com>
2024-10-28 20:18:00 -0600

    Adjusted van icon display

--- app/map.js
+++ app/map.js
@@ -32,8 +32,9 @@ map.on('load', async () => {
             console.log('Adding location marker:', location);
             const vanIcon = document.createElement('div');
             vanIcon.innerHTML = `
-                <svg width="30" height="30" viewBox="0 0 24 24" fill="#0000FF">
-                    <path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5H15V3H9v2H6.5c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z"/>
+                <svg width="40" height="24" viewBox="0 0 40 24" fill="#0000FF">
+                    <path d="M35 8h-3V6.5c0-3-2.5-5.5-5.5-5.5H5C2.2 1 0 3.2 0 6v12c0 1.1.9 2 2 2h2c0-2.2 1.8-4 4-4s4 1.8 4 4h15c0-2.2 1.8-4 4-4s4 1.8 4 4h3c1.1 0 2-.9 2-2v-6c0-2.2-1.8-4-4-4zM8 17c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3zm25 0c-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3-1.3-3-3-3zm5-7h-3V8h3v2z"/>
+                    <path d="M3 10h24v4H3z" fill="#FFFFFF"/>
                 </svg>`;
 
             new mapboxgl.Marker({
